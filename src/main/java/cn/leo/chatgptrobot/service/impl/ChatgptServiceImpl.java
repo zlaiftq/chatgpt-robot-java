d51a31e3144b6597131a3964534afbbd10d182dc
zhanglei <ZhangLei_Xiloer@163.com>
2023-05-07 21:54:04 +0800

    feat: 微信公众号对接限制最大返回量

--- src/main/java/cn/leo/chatgptrobot/service/impl/ChatgptServiceImpl.java
+++ src/main/java/cn/leo/chatgptrobot/service/impl/ChatgptServiceImpl.java
@@ -102,8 +102,8 @@ public class ChatgptServiceImpl implements ChatgptService {
             }
         }
         if (increment == 3) {
-            // 阻塞4.5s
-            Thread.sleep(4500);
+            // 阻塞4.6s
+            Thread.sleep(4600);
             if (StringUtils.isNotBlank(response)) {
                 return response;
             } else {
